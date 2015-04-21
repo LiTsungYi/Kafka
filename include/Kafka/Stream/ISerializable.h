d3871d81b9aaee13297955721a9589526ca9a915
Caesar <litsungyi@gmail.com>
2015-04-22 05:34:36 +0800

    Add ISerializable

---
+++ include/Kafka/Stream/ISerializable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+
+namespace Kafka
+{
+    struct IOutputStream;
+    struct IInputStream;
+
+    struct ISerializable
+    {
+        virtual void WriteToStream( std::shared_ptr<IOutputStream> stream ) = 0;
+        virtual void ReadFromStream( std::shared_ptr<IInputStream> stream ) = 0;
+    };
+} // namespace Kafka
