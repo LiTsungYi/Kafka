f5ad85edd27215ee6e02156e7f7ad69d436b557a
litsungyi <litsungyi@gmail.com>
2016-01-08 04:38:14 +0800

    Add Type Defines

---
+++ include/Kafka/Basic/Types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef long long Int64;
+typedef long Int32;
+typedef short Int16;
+typedef char Int8;
+typedef unsigned long long Uint64;
+typedef unsigned int Uint32;
+typedef unsigned short Uint16;
+typedef unsigned char Uint8;
+
